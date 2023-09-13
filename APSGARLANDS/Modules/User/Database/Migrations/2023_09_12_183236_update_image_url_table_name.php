8f6a38f29041d36d309f886abaa57a84f0c3ba1e
Logeswari-divya <logeswaridivya@gmail.com>
2023-09-13 10:52:48 +0530

    after added profile image upload

--- APSGARLANDS/Modules/User/Database/Migrations/2023_09_12_183236_update_image_url_table_name.php
+++ APSGARLANDS/Modules/User/Database/Migrations/2023_09_12_183236_update_image_url_table_name.php
@@ -16,7 +16,6 @@ class UpdateImageUrlTableName extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->text('image_url')->after('is_sso_fb')->nullable(); 
         });
-        //dd
     }
 
     /**
