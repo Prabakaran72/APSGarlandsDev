cff11c39e0a105ca06873e05a9ff808a124c8e7c
Prabakaran V <prabakaranpbk@gmail.com>
2023-11-10 12:05:10 +0530

    Recurring Order Checkout page component style updated

--- APSGARLANDS/Themes/Storefront/views/public/checkout/create/form/recurring_order.blade.php
+++ APSGARLANDS/Themes/Storefront/views/public/checkout/create/form/recurring_order.blade.php
@@ -1,27 +1,34 @@
 @if (setting('recurring_order_enabled'))
-    <div class="col-md-18" style="border: 2px solid green">
+    <div class="col-md-18">
         <div class="col-md-18">
             <label>
                 <br><br><input type="checkbox" v-model="form.isCheckedRecurringOrder"
                     @change="handleRecurringOrderPayment"> Recurring Order
             </label>
         </div>
-        <div class="col-md-18 row" v-if="form.isCheckedRecurringOrder">
+        <div class="col-md-18" v-if="form.isCheckedRecurringOrder">
             <div class="form-group d-flex">
-                <div class="form-label"><label>Subscription Date</label></div>
-                <div class="form-input">
+                <div class="form-label col-md-12 pr-2 pl-3"><label>Subscription Date(s)</label></div>
+                <div class="form-input pl-0">
                     <datepicker v-model="form.recurring_order_dates" :multiple="true" :range="false"
                         :disabled-date="RecurringDisabledDates" ref="recurring_order_dates">
                     </datepicker>
                 </div>
             </div>
-            <div class="col-md-18 row">
-                <div class="form-group">
-                    <div class="col-md-6"><label>Expected Delivery Time<span>*</span></label></div>
-                    <div class="col-md-6"><input type="time" id="recurring_time" class="form-control"
-                            v-model="form.recurring_time" ref="recurring_time"></div>
+            <div class="form-group d-flex">
+                <div class="form-label col-md-12 pr-2 pl-3"><label>Expected Delivery Time<span>*</span></label>
                 </div>
+                <div class="form-input pl-0"><input type="time" id="recurring_time"
+                        class="form-control mx-input pr-2" v-model="form.recurring_time" ref="recurring_time"></div>
             </div>
+
+            <div class="form-group d-flex">
+                <div class="form-label col-md-12 pr-2 pl-3"><label>Selected Order Date<span>*</span></label>
+                </div>
+                <div class="form-input pl-0"><input type="time" id="recurring_time"
+                        class="form-control mx-input pr-2" v-model="form.recurring_time" ref="recurring_time"></div>
+            </div>
+
         </div>
     </div>
 @endif
